09e2a6133078528b2215a8132016d7380aff7b30
Luana Passafaro <luana.c.passafaro@gmail.com>
2024-04-25 19:14:04 -0300

    carrito: private properties

--- carrito_poo.php
+++ carrito_poo.php
@@ -3,11 +3,26 @@
 date_default_timezone_set('America/Argentina/Buenos_Aires');
 
 class Cliente {
-    public $dni;
-    public $nombre;
-    public $correo;
-    public $telefono;
-    public $descuento;
+    private $dni;
+    private $nombre;
+    private $correo;
+    private $telefono;
+    private $descuento;
+
+    public function getDNI() { return $this->dni; }
+    public function setDNI($valor) { $this->dni = $valor; }
+
+    public function getNombre() { return $this->nombre; }
+    public function setNombre($valor) { $this->nombre = $valor; }
+
+    public function getCorreo() { return $this->correo; }
+    public function setCorreo($valor) { $this->correo = $valor; }
+
+    public function getTelefono() { return $this->telefono; }
+    public function setTelefono($valor) { $this->telefono = $valor; }
+
+    public function getDescuento() { return $this->descuento; }
+    public function setDescuento($valor) { $this->descuento = $valor; }
 
     public function __construct() {
         $this->descuento = 0;
@@ -24,13 +39,29 @@ class Cliente {
 }
 
 class Producto {
-    public $cod;
-    public $nombre;
-    public $precio;
-    public $descripcion;
-    public $iva;
+    private $cod;
+    private $nombre;
+    private $precio;
+    private $descripcion;
+    private $iva;
 
-    public function __construct() {
+    public function getCod() { return $this->cod; }
+    public function setCod($valor) { $this->cod = $valor; }
+
+    public function getNombre() { return $this->nombre; }
+    public function setNombre($valor) { $this->nombre = $valor; }
+
+    public function getPrecio() { return $this->precio; }
+    public function setPrecio($valor) { $this->precio = $valor; }
+
+    public function getDescripcion() { return $this->descripcion; }
+    public function setDescripcion($valor) { $this->descripcion = $valor; }
+
+    public function getIVA() { return $this->iva; }
+    public function setIVA($valor) { $this->iva = $valor; }
+
+    public function __construct($cod) {
+        $this->cod = $cod;
         $this->precio = 0;
         $this->iva = 0;
     }
@@ -50,12 +81,13 @@ class Producto {
 }
 
 class Carrito {
-    public $cliente;
-    public $aProductos;
-    public $subtotal;
-    public $total;
+    private $cliente;
+    private $aProductos;
+    private $subtotal;
+    private $total;
 
-    public function __construct() {
+    public function __construct($cliente) {
+        $this->cliente = $cliente;
         $this->aProductos = [];
         $this->subtotal = 0;
         $this->total = 0;
@@ -63,21 +95,21 @@ class Carrito {
 
     public function cargarProducto($producto) {
         $this->aProductos[] = $producto;
-        $this->subtotal += $producto->precio;
+        $this->subtotal += $producto->getPrecio();
         $this->total += $producto->calcularPrecioConIva();
     }
 
     public function calcularTotalConDescuento() {
-        return $this->total * (1 - $this->cliente->descuento);
+        return $this->total * (1 - $this->cliente->getDescuento());
     }
 
     public function imprimirTicket() {
         echo "<tr><th scope=\"row\">Fecha</th><td>" . date("d/m/Y H:i:s") . "</td></tr>";
-        echo "<tr><th scope=\"row\">DNI</th><td>" . $this->cliente->dni . "</td></tr>";
-        echo "<tr><th scope=\"row\">Nombre</th><td>" . $this->cliente->nombre . "</td></tr>";
+        echo "<tr><th scope=\"row\">DNI</th><td>" . $this->cliente->getDNI() . "</td></tr>";
+        echo "<tr><th scope=\"row\">Nombre</th><td>" . $this->cliente->getNombre() . "</td></tr>";
         echo "<tr><th scope=\"col\" colspan=\"2\">Productos:</th></tr>";
         foreach ($this->aProductos as $prod) {
-            echo "<tr><td>" . $prod->nombre . "</td><td>$" . number_format($prod->precio, 2, ",", ".") . "</td></tr>";
+            echo "<tr><td>" . $prod->getNombre() . "</td><td>$" . number_format($prod->getPrecio(), 2, ",", ".") . "</td></tr>";
         }
         echo "<tr><th scope=\"row\">Subtotal s/IVA:</th><td>$" . number_format($this->subtotal, 2, ",", ".") . "</td></tr>";
         echo "<tr><th scope=\"row\">TOTAL:</th><td>$" . number_format($this->calcularTotalConDescuento(), 2, ",", ".") . "</td></tr>";
@@ -86,28 +118,25 @@ class Carrito {
 
 //Programa
 $cliente1 = new Cliente();
-$cliente1->dni = "34765456";
-$cliente1->nombre = "Bernabé";
-$cliente1->correo = "bernabe@gmail.com";
-$cliente1->telefono = "+541188598686";
-$cliente1->descuento = 0.1;
+$cliente1->setDni("34765456");
+$cliente1->setNombre("Bernabé");
+$cliente1->setCorreo("bernabe@gmail.com");
+$cliente1->setTelefono("+541188598686");
+$cliente1->setDescuento(0.1);
 
-$producto1 = new Producto();
-$producto1->cod = "AB8767";
-$producto1->nombre = "Notebook 15\" HP";
-$producto1->descripcion = "Esta es una computadora HP";
-$producto1->precio = 30800;
-$producto1->iva = 10.5;
+$producto1 = new Producto("AB8767");
+$producto1->setNombre("Notebook 15\" HP");
+$producto1->setDescripcion("Esta es una computadora HP");
+$producto1->setPrecio(30800);
+$producto1->setIva(10.5);
 
-$producto2 = new Producto();
-$producto2->cod = "QWR579";
-$producto2->nombre = "Heladera Whirlpool";
-$producto2->descripcion = "Esta es una heladera no frost";
-$producto2->precio = 76000;
-$producto2->iva = 21;
+$producto2 = new Producto("QWR579");
+$producto2->setNombre("Heladera Whirlpool");
+$producto2->setDescripcion("Esta es una heladera no frost");
+$producto2->setPrecio(76000);
+$producto2->setIva(21);
 
-$carrito = new Carrito();
-$carrito->cliente = $cliente1;
+$carrito = new Carrito($cliente1);
 $carrito->cargarProducto($producto1);
 $carrito->cargarProducto($producto2);
 
